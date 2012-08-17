867db6e0c6a71c8c6053d805ea8095923cd099b7
Ritch_Chan <ritchchan@hotmail.com>
2012-08-17 17:46:59 -0400

    - Edit on Trainer.

--- JSFWebSite/src/rt/beans/User.java
+++ JSFWebSite/src/rt/beans/User.java
@@ -3,10 +3,37 @@ package rt.beans;
 public class User {
 		  private String name;
 		  private String password;
+		  private String username;
+		  private int age;
+		  
+		  public User()
+		  {	  
+		  }
+		  
+		  public String getUsername()
+		  {
+			  return username;
+		  }
+		  
+		  public void setUsername(String username)
+		  {
+			  this.username = username;
+		  }
+		  
+		  public int getAge()
+		  {
+			  return age;
+		  }
+		  
+		  public void setAge(int age)
+		  {
+			  this.age = age;
+		  }
 		  
 		  public String getName() {
 		    return name;
 		  }
+		  
 		  public void setName(String name) {
 		    this.name = name;
 		  }
